2a3bc93bfaf6495537657bdd58c30bb3ca2e76d7
mczerniewicz <m.czerniewicz13@gmail.com>
2020-04-01 17:29:51 +0200

    Notatki

---
+++ cw4.py
@@ -0,0 +1,14 @@
+class Osoba:
+    pass
+
+
+#kazda klasa dziedziczy po klasie object mimo ze tego nie widac
+
+tadek = Ososba();
+tadek.imie="Tadeusz"
+print(tadek.imie)
+
+#metoda musi miec w pierwszym miejscu atrybutu napis self (self,z)
+
+
+tadek = Osoba();
